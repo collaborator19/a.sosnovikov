31ccbde0d9e8bc8958bb6553f69e9b3cb0b4241e
collaborator19 <also7@yandex.ru>
2016-04-14 15:42:31 +0600

    Лекция 1. Выполненные 3 первых задания - отправленные на github.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввести с консоли 2 числа. Вывести их сумму на консоль.
+            /*
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a1+b1);
+            */
+            /*
+            //Ввести с консоли 2 числа: n и i. Вывести значение i-ого бита числа n. 
+            Console.Write("n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("n в двоичной системе:");
+            Console.WriteLine(Convert.ToString(n, 2));
+            Console.WriteLine();
+
+            Console.Write("i=");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Значение i-ого бита числа n:");
+            System.Console.WriteLine((n>>i)&1);
+            */
+            /*
+            //Ввести с консоли число. Обнулить последний бит этого числа. Вывести на консоль.
+            Console.Write("a3=");
+            int a3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("В двоичной системе:");
+            Console.WriteLine(Convert.ToString(a3, 2));
+            Console.WriteLine();
+
+            Console.WriteLine("После обнуления последнего бита.");
+            Console.WriteLine();
+
+            Console.WriteLine("В двоичной системе:");
+            System.Console.WriteLine(Convert.ToString(a3 >> 1, 2));
+            Console.WriteLine();
+
+            Console.WriteLine("Число:");
+            System.Console.WriteLine(a3 >> 1);
+            */
+
+            //
+            Console.Write("N=");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=0; i < N; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
