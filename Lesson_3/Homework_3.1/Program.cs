44ea08e6967b67a2d3ec2e7822b7b20f53f2028f
collaborator19 <also7@yandex.ru>
2016-05-12 03:42:52 +0600

    Выполнил домашнюю работу третьей лекции.

---
+++ Lesson_3/Homework_3.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Считать строку с консоли. Создать словарь, где ключом будет
+            //символ строки, а значением - количество данных символов в
+            //считанной строке.
+            Console.WriteLine("Введите строку с консоли:");
+            string stringFromConsole = Console.ReadLine();
+
+            int lenghtStringFromConsole = stringFromConsole.Length;
+            Console.WriteLine("\n" + "Длина строки:" + "\n" + lenghtStringFromConsole + "\n");
+
+            for (int i = 0; i < lenghtStringFromConsole; i++)
+            {
+                Dictionary<char, int> dictionaryStringFromConsole = new Dictionary<char, int>();
+                dictionaryStringFromConsole.Add(stringFromConsole[i], lenghtStringFromConsole);
+
+                Console.WriteLine("Ключ: " + stringFromConsole[i] + " Значение: " + dictionaryStringFromConsole[stringFromConsole[i]]);
+            }
+        }
+    }
+}
---
+++ Lesson_3/Homework_3.2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Считывать с консоли числа, пока не будет введено число “-1”, среди
+            //введенных чисел вывести все дублирующиеся.
+            int injectedNumbers = 1;
+            List<int> integersAll = new List<int>();
+            List<int> integersDuplicate = new List<int>();
+
+            Console.WriteLine("Введите числа с консоли с нажатием Enter после каждого введённого числа:");
+
+            for (int i = 0; i < injectedNumbers; i++)
+            {
+                int integerFromConsole = Convert.ToInt32(Console.ReadLine());
+
+                if (integersAll.Contains(integerFromConsole))
+                {
+                    integersDuplicate.Add(integerFromConsole);
+                }
+                else
+                {
+                    integersAll.Add(integerFromConsole);
+                }
+
+                if (integerFromConsole != -1)
+                {
+                    injectedNumbers++;
+                }
+                else
+                {
+                    if (integersDuplicate.Count == 0)
+                    {
+                        Console.WriteLine("Повторяющихся чисел среди введённых нет.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n" + "Дублируются следующие введёные числа:");
+                        for (int j = 0; j < integersDuplicate.Count; j++)
+                        {
+                            Console.WriteLine(integersDuplicate[j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
